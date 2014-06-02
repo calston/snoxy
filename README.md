3fac32b3a278077e26f8082f9e4e70edd56c4a93
Colin Alston <colin.alston@gmail.com>
2014-06-02 15:54:40 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 snoxy
 =====
 
-An authenticating HTML wrapper for crap services
+An authenticating HTML wrapper for crap services, like Kibana and Munin who still to this day have not gotten off their ass and realised "Hey, maybe sysadmins need to secure their internal tools better than clear-text HTTP basic auth"
---
+++ snoxy/wrapper/views.py
@@ -0,0 +1,54 @@
+import httplib, urllib
+try:
+    from urllib.parse import urlparse
+except:
+    from urlparse import urlparse
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.forms import CheckboxSelectMultiple, ValidationError
+from django.db.models import Sum
+from django.conf import settings
+
+
+@login_required
+def index(request):
+
+    url = urlparse(settings.SNOXY_HOST)
+
+    if url.scheme=='http':
+        conn = httplib.HTTPConnection(url.hostname, url.port)
+    elif url.scheme=='https':
+        conn = httplib.HTTPSConnection(url.hostname, url.port)
+
+    if request.GET:
+        path = '%s?%s' % (request.path, urllib.urlencode(request.GET))
+    else:
+        path = request.path
+
+    conn.request("GET", path)
+
+    response = conn.getresponse()
+
+    ct = response.getheader('Content-Type')
+    content = response.read()
+
+    if 'text/html' in ct:
+        content = content.replace(settings.SNOXY_HOST, '')
+
+    proxy_response = HttpResponse(
+        content,
+        content_type=response.getheader('Content-Type'),
+        status=response.status,
+        reason=response.reason
+    )
+    
+    headers = response.getheaders()
+
+    for k, v in headers:
+        if k in ['location', 'set-cookie', 'date', 'cache-control', 'content-type']:
+            proxy_response[k] = v.replace(settings.SNOXY_HOST, '')
+
+    return proxy_response
